af324e4074b83358358992f899f0e81d305c5863
Neil Barduson <barduson.neil@gmail.com>
2018-02-07 14:35:35 -0600

    added coin controller along with createWallet function and a createBTCTestWallet function

--- server/controllers/coin.js
+++ server/controllers/coin.js
@@ -1,3 +1,6 @@
+// import { ObjectId } from '../../../../Library/Caches/typescript/2.6/node_modules/@types/bson';
+const ObjectId = require("mongodb").ObjectId;
+
 const bitcore = require('bitcore-lib')
 
 // creates a testnet btc wallet and returns an object with its address, publicKey, and privateKey
@@ -21,4 +24,34 @@ const createBTCTestWallet = () => {
   return coinObj;
 }
 
-module.exports = { createBTCTestWallet };
+const createWallet = (req, res) => {
+  const { coin } = req.params;
+  
+  // instantiate a null wallet
+  let wallet;
+
+  // create wallet for coin
+  switch (coin) {
+    case 'btc_test':
+      wallet = createBTCTestWallet();
+      break;
+    default:
+      // throw error of invalid coin
+      res.json({ success: false, message: "please provide valid coin in url parameters" });
+  }
+
+  // hash the privateKey for the wallet
+  wallet.privateKey = bcrypt.hash(wallet.privateKey, 11);
+
+  // store coin in user document
+  // TODO: do we want to check if the user has already has a coins wallet?
+  //  this instance we do not check and assume they werent given an option
+  User.findOneAndUpdate(
+    { _id: ObjectId(req.user.id) }, 
+    { $addToSet: { wallets: wallet } }
+  )
+  .then(result => res.json({ success: true, message: 'wallet created!' }))
+  .catch(err => res.json(err))
+}
+
+module.exports = { createWallet };
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,11 +1,14 @@
 const userController = require("../controllers/user.js");
 const authController = require("../controllers/auth");
+const coinController = require("../controllers/coin");
 
 module.exports = app => {
   app.route("/register").post(userController.createUser);
   app
     .route("/signin")
     .post(authController.requireSignIn, authController.signIn);
+
+  app.route("/create-wallet/:coin").post(authController.requireAuth, coinController.createWallet);
 
   // Route to test Auth, can be removed if needed
   app.route("/users").get(authController.requireAuth, userController.getUsers);
