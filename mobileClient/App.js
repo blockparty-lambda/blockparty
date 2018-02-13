c6668566764759eec6f601cd3951515ca62cfe34
Joe Roland <joroland@gmail.com>
2018-02-12 19:19:25 -0500

    Implemented frontend Auth flow, and added expiration to JWTs

--- mobileClient/App.js
+++ mobileClient/App.js
@@ -1,42 +1,60 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { StackNavigator } from 'react-navigation';
-import Register from './components/Register';
-import SignIn from './components/SignIn';
-import Home from './components/Home';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { StackNavigator } from "react-navigation";
+import Register from "./components/Register";
+import SignIn from "./components/SignIn";
+import Home from "./components/Home";
 // import RootNavigation from './navigation/RootNavigation';
-import MainTabNavigator from './navigation/MainTabNavigator';
+// import MainTabNavigator from "./navigation/MainTabNavigator";
+import { createRootNavigator } from "./router";
+import { isSignedIn } from "./auth";
 
-class App extends React.Component {
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      signedIn: false,
+      checkedSignIn: false
+    };
+  }
+
+  async componentWillMount() {
+    isSignedIn()
+      .then(res => this.setState({ signedIn: res, checkedSignIn: true }))
+      .catch(error => alert("An error occurred"));
+  }
+
   render() {
-    return (
-      <View style={styles.container}>
-        {/* <SignIn /> */}
-        {/* <RootNavigation /> */}
-      </View>
-    );
+    const { signedIn, checkedSignIn } = this.state;
+    if (!checkedSignIn) {
+      return null;
+    }
+
+    const Layout = createRootNavigator(signedIn);
+    return <Layout />;
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
 
 // the first index of the stack navigator will be rendered first in the app
 // How do we set up the app so it checks if the user is signed in into react and
 // determines to render signin screen (not signed in) or home screen (signed in)
-const Routes = StackNavigator({
-  // Navbar: { screen: Navbar },
-  Main: { screen: MainTabNavigator },
-  SignIn: { screen: SignIn },
-  Register: { screen : Register },
-  // Main: { screen: MainTabNavigator }
-  // Home: { screen: Home },
-});
+// const Routes = StackNavigator({
+//   // Navbar: { screen: Navbar },
+//   Main: { screen: MainTabNavigator },
+//   SignIn: { screen: SignIn },
+//   Register: { screen: Register }
+//   // Main: { screen: MainTabNavigator }
+//   // Home: { screen: Home },
+// });
 
-export default Routes;
+// export default Routes;
