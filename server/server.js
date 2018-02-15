1c2d886485c8702e0b9872092a723f636567fa26
Joe Roland <joroland@gmail.com>
2018-02-15 14:31:46 -0500

    Implemented handling received friend requests

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const passport = require("passport");
+const cors = require("cors");
 
 const routes = require("./routes/routes");
 
@@ -14,6 +15,7 @@ const port = process.env.PORT || 3000;
 
 const app = express();
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(passport.initialize());
