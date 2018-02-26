006c13af423ad84525332677fcf45b5f0a3d88ea
Neil Barduson <barduson.neil@gmail.com>
2018-02-26 14:31:31 -0600

    deleted duplicated files

--- controllers/auth.js
+++
@@ -1,8 +0,0 @@
-const { requireSignIn, requireAuth } = require("../services/passport");
-const getTokenForUser = require("../services/token");
-
-const signIn = (req, res) => {
-  res.send({ token: getTokenForUser(req.user) });
-};
-
-module.exports = { signIn, requireSignIn, requireAuth };
