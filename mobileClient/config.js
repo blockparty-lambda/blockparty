b278e93fb28014d9c652b5c770e69cc398d19169
Neil Barduson <barduson.neil@gmail.com>
2018-03-09 12:09:22 -0600

    updated app.json for expo

--- mobileClient/config.js
+++ mobileClient/config.js
@@ -1,7 +1,7 @@
 import { localip, remote_api_url } from "react-native-dotenv";
 
-const apiUrl = localip;
-// const apiUrl = remote_api_url;
+// const apiUrl = localip;
+const apiUrl = remote_api_url;
 
 module.exports = {
   apiUrl
